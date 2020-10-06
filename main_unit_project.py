44e82a7da73900b6ce6a09eed686d149eadee587
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-10-06 11:27:14 -0500

    Create main_unit_project.py

---
+++ main_unit_project.py
@@ -0,0 +1,22 @@
+import random
+import countdown
+
+
+print("What's up everybody! Welcome to MagicCheck")
+print("Here, you will enter tasks needed to be done in a Magic 8-ball.")
+print("Shake the Magic 8-ball to randomly pick which task to do next, and it will start your timer.")
+print("Set the timer for however many minutes you would like.")
+print("Once your time is up, click \"Yes\" or \"No\" to check off the task.")
+
+num_tasks = int(input("How many tasks will you be completing today? "))
+tasks = []
+for i in range(num_tasks):
+    task = str(input("What would you like to get done today? "))
+    tasks.append(task)
+
+shake = input("Enter shake to choose a task: ")
+if shake.lower() == "shake":
+    print(random.choice(tasks))
+
+
+
