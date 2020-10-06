43e5d2be2025f16c9ce2ad97d1204be31e09503a
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-10-06 10:14:59 -0500

    Secs -> Minutes Fixed

--- countdown.py
+++ countdown.py
@@ -5,20 +5,20 @@ import time
 
 
 def countdown(t):
+    converted_t = int(t) * 60
 
-    while t:
-        hours, remainder = divmod(t, 3600)
-        mins, secs = divmod(remainder, 60)
+    while converted_t >= 0:
+        hours, quotient = divmod(converted_t, 3600)
+        mins, secs = divmod(quotient, 60)
         timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
         print(timer, end="\r")
         time.sleep(1)
-        t -= 1
+        converted_t -= 1
 
-    print('Fire in the hole!!')
+    print('You failed to complete: print[list]')
 
 
 # input time in minutes
 t = input("Enter the time in minutes: ")
-
 # function call
 countdown(int(t))
