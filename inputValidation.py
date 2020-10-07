bac081d45eb10f0c710161190068ee1655ecbc9d
Aj Hardaway <ahardaway21@basecampcodingacademy.org>
2020-10-07 11:30:21 -0500

    validation loops for shake and start

--- inputValidation.py
+++ inputValidation.py
@@ -1,3 +1,16 @@
+valid_inputs = [
+    "shake",
+    "start",
+]
+
+
+def is_valid_input(start: str) -> bool:
+    for input in valid_inputs:
+        if start.lower() == input:
+            return True
+    return False
+
+
 def input_complete_valid(input_complete: str) -> str:
     """Return if the input is valid.
     """
@@ -8,9 +21,19 @@ def input_complete_valid(input_complete: str) -> str:
 
 
 def input_shake_valid(shake: str) -> str:
-    """Return if the input is shake 
+    """Return if the input is shake
     """
     if shake.lower() == "shake":
         print(random.choice(tasks))
     else:
         print("Invalid input. No problem, have a good day!")
+
+
+while True:
+    input_name = input("What is your name? ")
+    is_valid = is_valid_input(input_name)
+    if is_valid:
+        print("If your name is valid. ")
+        break
+    else:
+        print("If your name is invalid.")
